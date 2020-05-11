4caa5d0b367dc174762adba9893290697920aca5
michaildoukas <michail.doukas@gmail.com>
2020-05-11 19:51:36 +0100

    faceforensicspp

--- scripts/train/finetune_on_target.sh
+++ scripts/train/finetune_on_target.sh
@@ -3,6 +3,6 @@ dataset_name=$2
 
 python train.py --checkpoints_dir checkpoints/$dataset_name \
                 --target_name $target_name \
-                --name head2head_finetuned_$target_name \
+                --name head2head_$target_name \
                 --dataroot datasets/$dataset_name/dataset \
                 --load_pretrain checkpoints/faceforensicspp/head2head
