83d87b91d6e35f78e3c4fed59f8e7c6dcf368d81
michaildoukas <michail.doukas@gmail.com>
2020-05-11 18:37:21 +0100

    download flownet2 checkpoints

--- scripts/download_files.py
+++ scripts/download_files.py
@@ -20,12 +20,12 @@ def main():
     print('Downloading models and files for face reconstruction and flownet2 \n')
     # Download urls (hardcoded)
     download_urls = \
-        [('preprocessing',  'https://www.dropbox.com/s/vzr7snb82n42mfw/files.zip?dl=1'),
+        [('preprocessing', 'https://www.dropbox.com/s/vzr7snb82n42mfw/files.zip?dl=1'),
          ('preprocessing', 'https://www.dropbox.com/s/uzqz2p9fw3ps7pk/models.zip?dl=1'),
          ('models/flownet2_pytorch', 'https://www.dropbox.com/s/ns7i26gpmhtnoxe/FlowNet2_checkpoint.pth_faceflow.tar?dl=1'),
          ('models/flownet2_pytorch', 'https://www.dropbox.com/s/55uwylzmth795xh/FlowNet2_checkpoint.pth.tar?dl=1')]
     for i, url in enumerate(download_urls):
-        fpath = os.path.join(dir, url[1].split('/')[-1].split('?')[0])
+        fpath = os.path.join(url[0], url[1].split('/')[-1].split('?')[0])
         if not os.path.exists(fpath):
             bar = MyProgressBar('Downloading file %d/%d' % (i+1,
                                 len(download_urls)))
