bc35b4e7bfd85868ea8c1d0007a4d66184ac8eb6
michaildoukas <michail.doukas@gmail.com>
2020-05-20 12:12:49 +0100

    test finetuned scripts

---
+++ scripts/test/test_self_reenactment_on_target_finetuned.sh
@@ -0,0 +1,14 @@
+target_name=$1
+dataset_name=$2
+epoch=latest # hardcoded
+
+python test.py --checkpoints_dir checkpoints/$dataset_name \
+               --results_dir results/$dataset_name \
+               --target_name $target_name \
+               --name head2head_finetuned_$target_name \
+               --dataroot datasets/$dataset_name/dataset \
+               --which_epoch $epoch
+
+results_dir=results/$dataset_name/head2head_finetuned_${target_name}/${epoch}_epoch/test/${target_name}
+
+python scripts/images_to_video.py --results_dir $results_dir --output_mode all
