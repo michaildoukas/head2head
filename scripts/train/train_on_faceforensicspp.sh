f8db8588f52f165c69d0a75b1ebf9bd1440059b6
michaildoukas <michail.doukas@gmail.com>
2020-05-24 11:24:37 +0100

    faceforensicspp scripot

--- scripts/train/train_on_faceforensicspp.sh
+++ scripts/train/train_on_faceforensicspp.sh
@@ -1,4 +1,4 @@
 python train.py --checkpoints_dir checkpoints/faceforensicspp \
                 --name head2head_faceforensicspp \
-                --dataroot datasets/faceforensicspp \
+                --dataroot datasets/faceforensicspp/dataset \
                 --niter 15 --niter_decay 0
