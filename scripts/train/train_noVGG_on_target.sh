2fdb85889b14a367901a14965249429d2fa3a28d
michaildoukas <michail.doukas@gmail.com>
2020-05-25 17:59:14 +0100

    nihup

--- scripts/train/train_noVGG_on_target.sh
+++ scripts/train/train_noVGG_on_target.sh
@@ -2,9 +2,9 @@ target_name=$1
 dataset_name=$2
 gpu=$3
 
-python train.py --checkpoints_dir checkpoints/$dataset_name \
+nohup python train.py --checkpoints_dir checkpoints/$dataset_name \
                 --target_name $target_name \
                 --name head2head_noVGG_$target_name \
                 --dataroot datasets/$dataset_name/dataset \
                 --no_vgg \
-                --gpu_ids $gpu
+                --gpu_ids $gpu >/dev/null 2>&1 &
