b00dcaa8c379fffb44f14f44f0ff4b3865d9bd2a
michaildoukas <michail.doukas@gmail.com>
2020-05-30 10:43:36 +0100

    finetune script gpu args

--- scripts/train/finetune_on_target_numseq.sh
+++ scripts/train/finetune_on_target_numseq.sh
@@ -1,10 +1,12 @@
 target_name=$1
 dataset_name=$2
 numseq=$3
+gpu=$4
 
-python train.py --checkpoints_dir checkpoints/$dataset_name \
+nohup python train.py --checkpoints_dir checkpoints/$dataset_name \
                 --target_name $target_name \
                 --name head2head_finetuned_${target_name}_${numseq} \
                 --dataroot datasets/$dataset_name/dataset \
                 --load_pretrain checkpoints/faceforensicspp/head2head_faceforensicspp \
-                --max_n_sequences $numseq
+                --max_n_sequences $numseq \
+                --gpu_ids $gpu >/dev/null 2>&1 &
